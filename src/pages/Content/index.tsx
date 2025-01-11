fe049af9fd2399a3b4861dcc51514f3e9aceb22b
1240791903@qq.com <1240791903@qq.com>
2025-01-11 21:03:51 +0800

    modify safari bottom bar problem

--- src/pages/Content/index.tsx
+++ src/pages/Content/index.tsx
@@ -1,10 +1,11 @@
 import ContentMain from "@/components/ContentMain/index.tsx";
 import ContentTop from "@/components/ContentTop/index.tsx";
 import styles from "./index.module.css";
+import clsx from "clsx";
 
 const Content = () => {
   return (
-    <div className={styles["main-content"]}>
+    <div className={clsx(styles["main-content"])}>
       <ContentTop />
       <ContentMain />
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+// import legacy from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,6 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+
+  server: { host: "0.0.0.0" },
 });
